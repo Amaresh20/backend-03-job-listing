2ac1bfd53e2225a1a430cbf1c49141889278c1b0
Amaresh20 <sahooa218@gmail.com>
2023-10-15 18:37:23 +0530

    adding backend routes

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+const jobRoutes = require("./routes/job");
+const dotenv = require("dotenv");
+dotenv.config();
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+//app.use(bodyParser.json());
+app.use(authRoutes);
+app.use(jobRoutes);
+
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.json({
+    title: "Active",
+    message: "All good!!",
+  });
+});
+app.get("/health", (req, res) => {
+  res.json({
+    service: "job-listing-server",
+    status: "active",
+    time: new Date(),
+  });
+});
+
+app.listen(process.env.PORT, () => {
+  mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() =>
+      console.log(`server running at http://localhost:${process.env.PORT}`)
+    )
+    .catch((error) => console.log(error));
+});
